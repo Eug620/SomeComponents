379cc7af388cc752698c8d3edc57380c25261edd
yeyuhang <yeyuhang@appicplay.com>
2021-10-27 19:39:17 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import RouterConfig from './router'
 
 ReactDOM.render(
-  <App />,
+  <RouterConfig/>,
   document.getElementById('root')
 );
 
